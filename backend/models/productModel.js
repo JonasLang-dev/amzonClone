d97ebe67d1ab98be24399d89f14f983581fd8142
supercutcat <supercutcat@outlook.com>
2021-05-06 22:35:04 +0800

    Create Product

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -7,6 +7,7 @@ const productSchema = new Schema(
     image: { type: String, required: true },
     brand: { type: String, required: true },
     description: { type: String, required: true },
+    category: { type: String, required: true },
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
--- frontend-copy/src/store.js
+++ frontend-copy/src/store.js
@@ -8,6 +8,7 @@ import {
   orderMineListReducer,
 } from "./reducers/orderReducers";
 import {
+  productCreateReducer,
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducers";
@@ -46,6 +47,7 @@ const reducer = combineReducers({
   orderMineList: orderMineListReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  productCreate: productCreateReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
